4609eabb4df5ba49572c9d85b53350442950c649
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2022-09-28 00:35:41 -0400

    Got eslint to work and added .eslintignore.

--- src/commands/Info/ping.ts
+++ src/commands/Info/ping.ts
@@ -4,6 +4,7 @@ export default new Command({
     name: 'ping',
     description: 'replies with pong',
     run: async ({ interaction }) => {
+        var x = 7
         interaction.followUp("Pong");
     }
 });
