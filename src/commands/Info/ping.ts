4ed8cda77ccc731d1236c5b2d70844e9757b5bc2
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2022-09-28 03:12:32 -0400

    modified .eslintrc.json

--- src/commands/Info/ping.ts
+++ src/commands/Info/ping.ts
@@ -1,10 +1,9 @@
 import { Command } from "../../structures/Command";
 
 export default new Command({
-    name: 'ping',
-    description: 'replies with pong',
+    name: "ping",
+    description: "replies with pong",
     run: async ({ interaction }) => {
-        var x = 7
         interaction.followUp("Pong");
     }
 });
